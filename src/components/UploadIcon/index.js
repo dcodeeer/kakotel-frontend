8025eec933b677242116921a4e5db3b56354d887
dcoder <dcodeeer@proton.me>
2024-06-17 15:55:06 +0500

    beta 0.25.0

--- src/components/UploadIcon/index.js
+++ src/components/UploadIcon/index.js
@@ -47,10 +47,12 @@ export const UploadIcon = ({ name, setIcon }) => {
     <div className={isFileSelected ? `upload-icon selected` : `upload-icon`}>
       {isLoading ? <LoaderModal /> : '' }
       <input ref={inputRef} id='upload-icon' type='file' name={name} onChange={onChange} accept="image/png, image/gif, image/jpeg" />
-      <label className='button' htmlFor='upload-icon'>Загрузить</label>
+      <label className='button-icon' htmlFor='upload-icon'>
+        <span>Загрузить иконку</span>
+        <svg className="ionicon" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M320 367.79h76c55 0 100-29.21 100-83.6s-53-81.47-96-83.6c-8.89-85.06-71-136.8-144-136.8-69 0-113.44 45.79-128 91.2-60 5.7-112 43.88-112 106.4s54 106.4 120 106.4h56" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/><path d="M320 255.79l-64-64-64 64M256 448.21V207.79" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg>
+      </label>
 
-      <div class='image-container' onClick={deleteIcon}>
-        
+      <div className='image-container' onClick={deleteIcon}>
         <div className='delete'><svg className="ionicon" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/><path d="M80 112h352" stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32"/><path d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32"/></svg></div>
         <img id='preview' />
       </div>
--- src/pages/dashboard/Categories/index.js
+++ src/pages/dashboard/Categories/index.js
@@ -1,36 +1,83 @@
-import { UploadIcon } from 'components/UploadIcon';
-
-import './styles.scss';
 import { useState } from 'react';
+import { UploadIcon } from 'components/UploadIcon';
+import './styles.scss';
+import axios from 'axios';
+import { useLoaderData } from 'react-router-dom';
 
 export const DashboardCategories = () => {
   const [icon, setIcon] = useState('');
   const [name, setName] = useState('');
 
+  const { categories } = useLoaderData();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const req = await axios.post('/estates/category', { name, icon });
+      if (req.status === 200) {
+        console.log('success');
+      } else {
+        console.log('error')
+      }
+    } catch (e) {
+      console.log('error')
+    }
+  };
+
   return (
     <div className='dashboard-categories'>
       <div className='dashboard-title'>Категории</div>
 
-      <div className='table'>
-
-        <form className='row'>
-          <div></div>
+      <div className='add-new'>
+        <div className='dashboard-subtitle'>Добавить новое</div>
+        <form className='row' onSubmit={onSubmit}>
           <UploadIcon setIcon={setIcon} />
-          <input type='text' onChange={setName} />
+          <input type='text' onChange={(e) => setName(e.target.value)} placeholder='Название' />
+          <button className='button'>Добавить</button>
         </form>
+      </div>
+
+      <div className='list'>
+
+        <div className='dashboard-subtitle'>Список</div>
         
-        <div className='row'>
-          <div className='item'>ID</div>
-          <div className='item'>Иконка</div>
-          <div className='item'>Название</div>
+        <div className='table'>
+          <div className='row'>
+            <div className='item'>ID</div>
+            <div className='item'>Иконка</div>
+            <div className='item'>Название</div>
+          </div>
+
+          {categories.map((category, i) => (
+            <div className='row'>
+              <div className='item'>{category.id}</div>
+              <div className='item'>
+                <img src={process.env.REACT_APP_FILES_BASE_PATH + category.icon} />
+              </div>
+              <div className='item'>{category.name}</div>
+            </div>
+          ))}
         </div>
 
-        // foreach
       </div>
     </div>
   );
 };
 
 DashboardCategories.loader = async () => {
-  return {};
+  let output = {
+    categories: [],
+  };
+
+  try {
+    const req = await axios.get('/estates/categories');
+    if (req.status === 200) {
+      if (req.data) {
+        output.categories = req.data;
+      }
+    }
+  } catch (e) {}
+  
+  return output;
 };
